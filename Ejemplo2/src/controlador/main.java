743e9fec7d0ee3250e666568e552cb41894d1232
ikasle <ikasle@DAM1-15.arangoya.local>
2025-03-25 14:04:46 +0100

    V:1.0.3

--- Ejemplo2/src/controlador/main.java
+++ Ejemplo2/src/controlador/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Salchicha");
+		System.out.println("Aritza pihghgcha");
 
 	}
 
